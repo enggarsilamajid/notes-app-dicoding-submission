eff14fe71274bc8691f93e5acee9815ccd486322
Enggar Sila Majid <111524521+enggarsilamajid@users.noreply.github.com>
2026-02-20 09:52:22 +0700

    update search-bar.js

--- src/script/components/search-bar.js
+++ src/script/components/search-bar.js
@@ -158,7 +158,7 @@ class SearchBar extends HTMLElement {
         <form id="searchForm" class="search-form">
           <div class="form-group">
             <input id="name" name="name" type="search" required />
-            <label for="name">Sport club's name</label>
+            <label for="name">Note's Title</label>
           </div>
  
           <button>Search</button>
