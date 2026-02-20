efd1acebb436fde0afd46228c7674379e2299a1e
Enggar Sila Majid <111524521+enggarsilamajid@users.noreply.github.com>
2026-02-20 09:44:21 +0700

    update home.js, add app-bar.js and footer-bar.js

---
+++ src/script/view/home.js
@@ -0,0 +1,63 @@
+import Utils from '../utils.js';
+import Notes from '../data/local/notes.js';
+
+const home = () => {
+  const searchFormElement = document.querySelector('search-bar');
+  const noteListContainerElement = document.querySelector('#noteListContainer');
+  const noteQueryWaitingElement = noteListContainerElement.querySelector('.query-waiting');
+  const noteLoadingElement = noteListContainerElement.querySelector('.search-loading');
+  const noteListElement = noteListContainerElement.querySelector('club-list');
+
+  const showNotesData = (query) => {
+    showLoading();
+
+    const result = Notes.searchClub(query);
+    displayResult(result);
+
+    showNoteList();
+  };
+
+  const onSearchHandler = (event) => {
+    event.preventDefault();
+
+    const { query } = event.detail;
+    showNotesData(query);
+  };
+
+  const displayResult = (notes) => {
+    const noteItemElements = notes.map((note) => {
+      const noteItemElement = document.createElement('note-item');
+      noteItemElement.note = note;
+      return noteItemElement;
+    });
+
+    Utils.emptyElement(noteListElement);
+    noteListElement.append(...noteItemElements);
+  };
+
+  const showNoteList = () => {
+    Array.from(noteListContainerElement.children).forEach((element) => {
+      Utils.hideElement(element);
+    });
+    Utils.showElement(noteListElement);
+  };
+
+  const showLoading = () => {
+    Array.from(noteListContainerElement.children).forEach((element) => {
+      Utils.hideElement(element);
+    });
+    Utils.showElement(noteLoadingElement);
+  };
+
+  const showQueryWaiting = () => {
+    Array.from(noteListContainerElement.children).forEach((element) => {
+      Utils.hideElement(element);
+    });
+    Utils.showElement(noteQueryWaitingElement);
+  };
+
+  searchFormElement.addEventListener('search', onSearchHandler);
+  showQueryWaiting();
+};
+
+export default home;
