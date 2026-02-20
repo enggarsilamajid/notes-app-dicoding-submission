861550f1b81753488a1907e7980ca3583590d114
Enggar Sila Majid <111524521+enggarsilamajid@users.noreply.github.com>
2026-02-20 09:46:16 +0700

    update app-bar.js and footer-bar.js

--- src/script/components/footer-bar.js
+++ src/script/components/footer-bar.js
@@ -38,7 +38,7 @@ class FooterBar extends HTMLElement {
     this._shadowRoot.appendChild(this._style);
     this._shadowRoot.innerHTML += `      
       <div>
-        Club Finder &copy; 2023
+        enggarsilamajid &copy; 2026
       </div>
     `;
   }
