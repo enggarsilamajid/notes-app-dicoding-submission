a64b2aeed8bfede573be941d29f779a0ce272ed5
Enggar Sila Majid <111524521+enggarsilamajid@users.noreply.github.com>
2026-02-23 14:51:28 +0700

    update utils.js and index.html

--- src/script/utils.js
+++ src/script/utils.js
@@ -1,31 +1,18 @@
-class Utils {
-  static emptyElement(element) {
-    element.innerHTML = '';
-  }
-
-  static showElement(element) {
-    element.style.display = 'block'
-    element.hidden = false;
-  }
-
-  static hideElement(element) {
-    element.style.display = 'none';
+const Utils = {
+  hideElement(element) {
+    if (!element) return;
     element.hidden = true;
-  }
+  },
 
-  static isValidInteger(newValue) {
-    return Number.isNaN(newValue) || Number.isFinite(newValue);
-  }
+  showElement(element) {
+    if (!element) return;
+    element.hidden = false;
+  },
 
-  static formatDate(dateString) {
-    const date = new Date(dateString);
-
-    return new Intl.DateTimeFormat('id-ID', {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric',
-    }).format(date);
-  }
-}
+  emptyElement(element) {
+    if (!element) return;
+    element.innerHTML = '';
+  },
+};
 
 export default Utils;
