b9c51f78c4a7db1eadb72856e854d7adcdc8af98
Enggar Sila Majid <111524521+enggarsilamajid@users.noreply.github.com>
2026-02-23 17:27:56 +0700

    Delete src/script/components/index.js

--- src/script/components/index.js
+++
@@ -1,9 +0,0 @@
-import './app-bar.js';
-import './search-bar.js';
-import './footer-bar.js';
-import './note-list.js';
-import './note-item.js';
-import './note-detail.js';
-import './note-form.js';
-import './note-filter.js';
-import './loading-indicator.js';
