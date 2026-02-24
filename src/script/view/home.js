44d6e78c0dc0d3f187231d3f9b9887915b47e79c
Enggar Sila Majid <111524521+enggarsilamajid@users.noreply.github.com>
2026-02-24 11:41:55 +0700

    update showLoading()

--- src/script/view/home.js
+++ src/script/view/home.js
@@ -21,7 +21,9 @@ const home = () => {
 
   const showLoading = () => {
     hideAllChildren();
-    Utils.showElement(loadingElement);
+    if (loadingElement) {
+      Utils.showElement(loadingElement);
+    }
   }
 
   const hideAllChildren = () => {
