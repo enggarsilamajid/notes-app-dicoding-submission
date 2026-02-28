c15dce79db0a9d9645c09d42b880da45a1a96f7a
Enggar Sila Majid <111524521+enggarsilamajid@users.noreply.github.com>
2026-03-01 06:05:43 +0700

    Add files via upload

---
+++ src/script/components/note-item.js
@@ -0,0 +1,107 @@
+import Utils from "../utils.js";
+
+class NoteItem extends HTMLElement {
+  _shadowRoot = null;
+  _style = null;
+  _note = {
+    id: null,
+    title: null,
+    body: null,
+    createdAt: null,
+    archived: null,
+  };
+
+  constructor() {
+    super();
+
+    this._shadowRoot = this.attachShadow({ mode: 'open' });
+    this._style = document.createElement('style');
+  }
+
+  _emptyContent() {
+    this._shadowRoot.innerHTML = '';
+  }
+
+  set note(value) {
+    this._note = value;
+
+    // Render ulang
+    this.render();
+  }
+
+  get note() {
+    return this._note;
+  }
+
+  _updateStyle() {
+    this._style.textContent = `
+      :host {
+        display: block;
+        border-radius: 8px;
+        
+        box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.5);
+        overflow: hidden;
+      }
+ 
+      .note-info {
+        padding: 16px 24px;
+      }
+ 
+      .note-info__title h2 {
+        font-weight: lighter;
+      }
+
+      .note-info__date p {
+        font-size: .8em;
+      }
+
+      .note-info__description p {
+        display: -webkit-box;
+        margin-top: 10px;
+        
+        overflow: hidden;
+ 
+        text-overflow: ellipsis;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 5; /* number of lines to show */
+      }
+    `;
+  }
+
+  render() {
+    this._emptyContent();
+    this._updateStyle();
+
+    this._shadowRoot.appendChild(this._style);
+    this._shadowRoot.innerHTML += `
+      <div class="card">
+        
+        <div class="note-info">
+          <div class="note-info__title">
+            <h2>${this._note.title}</h2>
+          </div>
+          <div class="note-info__description">
+            <p>${this._note.body}</p>
+          </div>
+          <div class="note-info__date">
+            <p>${Utils.formatDate(this._note.createdAt)}</p>
+          </div>
+        </div>
+      </div>
+    `;
+
+    const card = this._shadowRoot.querySelector('.card');
+
+    card.addEventListener('click', () => {
+      this.dispatchEvent(
+        new CustomEvent('open-detail', {
+          detail: { id: this._note.id },
+          bubbles: true,
+          composed: true,
+        })
+      );
+    });
+  }
+}
+
+customElements.define('note-item', NoteItem);
