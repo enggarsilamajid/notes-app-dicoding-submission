444d0f22923224ee6ada9f0e58e258aa1d6cd530
Enggar Sila Majid <111524521+enggarsilamajid@users.noreply.github.com>
2026-03-01 05:50:27 +0700

    restart

--- src/script/components/index.js
+++
@@ -1,8 +0,0 @@
-import './app-bar.js';
-import './search-bar.js';
-import './footer-bar.js';
-import './note-list.js';
-import './note-item.js';
-import './note-detail.js';
-import './note-form.js';
-// import './components/loading-indicator.js';
