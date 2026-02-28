fa3b546a35fa1793caedc3f54edc9eba61de34b8
Enggar Sila Majid <111524521+enggarsilamajid@users.noreply.github.com>
2026-03-01 06:26:32 +0700

    update home.js

--- src/script/view/home.js
+++ src/script/view/home.js
@@ -1,109 +1,15 @@
-import Utils from '../utils.js';
-import NotesData from '../data/local/notes.js';
-import renderDetail from './detail.js';
-import renderAddForm from './add-note.js';
+import NotesApi from '../api/notes-api.js';
 
-const home = () => {
-  const addNoteButton = document.querySelector('#addNoteBtn');
-  const searchBarContainerElement = document.querySelector('#searchBarContainer');
-  const titleSectionElement = document.querySelector('.title-section');
+async function loadNotes() {
+  try {
+    const notes = await NotesApi.getNotes();
 
-  const searchBarElement = document.querySelector('search-bar');
-  const noteListContainerElement = document.querySelector('#noteListContainer');
-  const noteNotFoundElement =
-    noteListContainerElement.querySelector('.not-found');
-  const noteListElement =
-    noteListContainerElement.querySelector('note-list');
+    const noteListElement = document.querySelector('note-list');
+    noteListElement.notes = notes;
 
-  const hideAllChildren = () => {
-    Array.from(noteListContainerElement.children).forEach((element) => {
-      Utils.hideElement(element);
-    });
-  };
+  } catch (error) {
+    console.error('Gagal mengambil data', error);
+  }
+}
 
-  const displayResult = (notes) => {
-    Utils.emptyElement(noteListElement);
-
-    const noteItemElements = notes.map((note) => {
-      const noteItemElement = document.createElement('note-item');
-      noteItemElement.note = note;
-      return noteItemElement;
-    });
-
-    noteListElement.append(...noteItemElements);
-  };
-
-  const showNoteList = () => {
-    hideAllChildren();
-    Utils.showElement(noteListElement);
-  };
-
-  const showNotFound = () => {
-    hideAllChildren();
-    Utils.showElement(noteNotFoundElement);
-  };
-
-  const showNotes = (query = '') => {
-    const result = NotesData.searchNote(query);
-
-    if (!query) {
-      displayResult(result);
-      showNoteList();
-      return;
-    }
-
-    if (result.length === 0) {
-      showNotFound();
-      return;
-    }
-
-    displayResult(result);
-    showNoteList();
-  };
-
-  const onSearchHandler = (event) => {
-    const { query } = event.detail;
-    showNotes(query);
-  };
-
-  const returnToListView = () => {
-    searchBarContainerElement.classList.remove('view-hidden');
-    titleSectionElement.classList.remove('view-hidden');
-
-    showNotes();
-  };
-
-  // Initial load
-  searchBarElement.addEventListener('search', onSearchHandler);
-  showNotes();
-
-  // Open Detail
-  document.addEventListener('open-detail', (event) => {
-    const noteId = event.detail.id;
-    const selectedNote = NotesData.getNoteById(noteId);
-
-    renderDetail({
-      note: selectedNote,
-      container: noteListContainerElement,
-      searchBar: searchBarContainerElement,
-      titleSection: titleSectionElement,
-      noteList: noteListElement,
-      notFound: noteNotFoundElement,
-      returnToList: returnToListView,
-    });
-  });
-
-  // Open Add Form
-  addNoteButton.addEventListener('click', () => {
-    renderAddForm({
-      container: noteListContainerElement,
-      searchBar: searchBarContainerElement,
-      titleSection: titleSectionElement,
-      noteList: noteListElement,
-      notFound: noteNotFoundElement,
-      returnToList: returnToListView,
-    });
-  });
-};
-
-export default home;
+document.addEventListener('DOMContentLoaded', loadNotes);
