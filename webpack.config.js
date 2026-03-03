aa017cd5377fbbe8aa61a1867ec3710fd20a8511
Enggar Sila Majid <111524521+enggarsilamajid@users.noreply.github.com>
2026-03-03 07:36:08 +0000

    final

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
 
-  entry: './src/script/index.js',
+  entry: './src/app.js',
 
   output: {
     filename: 'bundle.js',
